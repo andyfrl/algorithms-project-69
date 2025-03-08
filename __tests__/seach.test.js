715a050cc6421134554886ce701f97c309c4b9eb
Andrey Frolov <andrey.frolov@revelsystems.com>
2025-03-08 19:06:43 +0300

    Rewrite to fuzzy search

--- __tests__/seach.test.js
+++ __tests__/seach.test.js
@@ -9,13 +9,23 @@ const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!
 const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
-const pattern = 'shoot';
-const expectedResult = ['doc2', 'doc1'];
-
-test('search', () => {
-  expect(search(docs, pattern)).toStrictEqual(expectedResult);
-});
+const pattern = {
+	oneWord: 'shoot',
+	multiWord: 'shoot at me',
+};
+const expectedResult = {
+	oneWord: ['doc2', 'doc1', 'doc3'],
+	multiWord: ['doc2', 'doc1', 'doc3'],
+};
 
 test('searchEmpty', () => {
-	expect(search([], pattern)).toStrictEqual([]);
+	expect(search([], pattern.oneWord)).toStrictEqual([]);
 })
+
+test('searchOneWord', () => {
+  	expect(search(docs, pattern.oneWord)).toStrictEqual(expectedResult.oneWord);
+});
+
+test('searchMultiWord', () => {
+	expect(search(docs, pattern.multiWord)).toStrictEqual(expectedResult.multiWord);
+});
--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 export default function search(inputArray, stringToken) {
-	let result = [];
-	const regex = new RegExp('\\w+', 'g');
-	const stringTerm = (stringToken.match(regex) || [])[0];
+	const regex = /\w+/g;
+	const stringTerm = (stringToken.match(regex) || []);
 
-	for (let i = 0; i < inputArray.length; i++) {
-		const numberOfMatchingItems = inputArray[i].text
-			.match(regex)
-			.filter((el) => el === stringTerm).length;
+	if (!inputArray.length || !stringTerm.length) return [];
 
-		if (numberOfMatchingItems) {
-			result.push({id: inputArray[i].id, relevance: numberOfMatchingItems});
+	let termHashMap = new Map();
+	stringTerm.forEach(element => termHashMap.set(element, 0));
+
+	const result = inputArray.map(({ id, text }) => {
+		let hasFullSetOfValues = true;
+		let wordSum = 0;
+
+		text.match(regex).forEach(matchingWord => {
+			if (termHashMap.has(matchingWord)) {
+				termHashMap.set(matchingWord, termHashMap.get(matchingWord) + 1);
+			}
+		});
+
+		termHashMap.forEach((value, key) => {
+			if (!value) hasFullSetOfValues = false;
+			wordSum += value;
+			termHashMap.set(key, 0);
+		});
+		
+		return {id, hasFullSetOfValues, wordSum};
+
+	});
+
+	// descending sort only
+	function comparator(a, b) {		
+		if (b.hasFullSetOfValues && !a.hasFullSetOfValues) return 1;
+		else if (a.hasFullSetOfValues === b.hasFullSetOfValues) {
+			return b.wordSum - a.wordSum;
+		} else {
+			return -1;
 		}
 	}
-
+		
 	return result
-		.sort((a, b) => b.relevance - a.relevance)
+		.sort(comparator)
 		.map(({id}) => id);
-
 }
