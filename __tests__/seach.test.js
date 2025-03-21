90dd4e798c7661c2c52fa5c29af1ab941f258947
Andrey Frolov <andrey.frolov@revelsystems.com>
2025-03-21 15:44:17 +0300

    Fix lint warnings

--- __tests__/seach.test.js
+++ __tests__/seach.test.js
@@ -1,8 +1,8 @@
 // @ts-check
 import path from 'path';
 import { expect, test } from 'vitest';
-import search from '../src/index.js';
 import { readFileSync } from 'node:fs';
+import search from '../src/index.js';
 
 /* -------- simple tests --------- */
 
@@ -11,30 +11,30 @@ const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
 const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const docs = [doc1, doc2, doc3];
 const pattern = {
-	oneWord: 'shoot',
-	multiWord: 'shoot at me',
-	multiWord2: 'shooter at me',
+  oneWord: 'shoot',
+  multiWord: 'shoot at me',
+  multiWord2: 'shooter at me',
 };
 const expectedResult = {
-	oneWord: ['doc2', 'doc1'],
-	multiWord: ['doc2', 'doc1'],
-	multiWord2: ['doc3', 'doc2'],
+  oneWord: ['doc2', 'doc1'],
+  multiWord: ['doc2', 'doc1'],
+  multiWord2: ['doc3', 'doc2'],
 };
 
 test('searchEmpty', () => {
-	expect(search([], pattern.oneWord)).toStrictEqual([]);
-})
+  expect(search([], pattern.oneWord)).toStrictEqual([]);
+});
 
 test('searchOneWord', () => {
-  	expect(search(docs, pattern.oneWord)).toStrictEqual(expectedResult.oneWord);
+  expect(search(docs, pattern.oneWord)).toStrictEqual(expectedResult.oneWord);
 });
 
 test('searchMultiWord', () => {
-	expect(search(docs, pattern.multiWord)).toStrictEqual(expectedResult.multiWord);
+  expect(search(docs, pattern.multiWord)).toStrictEqual(expectedResult.multiWord);
 });
 
 test('searchMultiWord2', () => {
-	expect(search(docs, pattern.multiWord2)).toStrictEqual(expectedResult.multiWord2);
+  expect(search(docs, pattern.multiWord2)).toStrictEqual(expectedResult.multiWord2);
 });
 
 /* -------- complex tests --------- */
@@ -42,27 +42,27 @@ test('searchMultiWord2', () => {
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
 const getDocumentText = (id) => {
-	const documentPath = getFixturePath(id);
-	const text = readFileSync(documentPath, 'utf8');
-	return {id, text};
-}
+  const documentPath = getFixturePath(id);
+  const text = readFileSync(documentPath, 'utf8');
+  return { id, text };
+};
 
 test('simpleSearch', () => {
-	const searchText = 'trash island';
-	const docIds = ['garbage_patch_NG', 'garbage_patch_ocean_clean', 'garbage_patch_wiki'];
+  const searchText = 'trash island';
+  const docIds = ['garbage_patch_NG', 'garbage_patch_ocean_clean', 'garbage_patch_wiki'];
 
-	const documentPaths = docIds.map(getDocumentText);
-	const result = search(documentPaths, searchText);
+  const documentPaths = docIds.map(getDocumentText);
+  const result = search(documentPaths, searchText);
 
-	expect(result).toEqual(docIds);
+  expect(result).toEqual(docIds);
 });
 
 test('search with spam, pretext', () => {
-	const searchText = 'the trash island is a';
-	const docIds = ['garbage_patch_NG', 'garbage_patch_ocean_clean', 'garbage_patch_wiki', 'garbage_patch_spam'];
+  const searchText = 'the trash island is a';
+  const docIds = ['garbage_patch_NG', 'garbage_patch_ocean_clean', 'garbage_patch_wiki', 'garbage_patch_spam'];
 
-	const documentPaths = docIds.map(getDocumentText);
-	const result = search(documentPaths, searchText);
+  const documentPaths = docIds.map(getDocumentText);
+  const result = search(documentPaths, searchText);
 
-	expect(result).toEqual(docIds);
+  expect(result).toEqual(docIds);
 });
--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
 export default function search(inputArray, stringToken) {
-	const regex = /\b[a-zA-Z]+(?:'[a-zA-Z]+)?(?!°[a-zA-Z])\b/g;
-	const stringTerm = (stringToken.toLowerCase().match(regex) || []);
-	let index = new Map();
-	let result = new Map();
-	let docWordCount = new Map();
+  const regex = /\b[a-zA-Z]+(?:'[a-zA-Z]+)?(?!°[a-zA-Z])\b/g;
+  const stringTerm = (stringToken.toLowerCase().match(regex) || []);
+  const index = new Map();
+  const result = new Map();
+  const docWordCount = new Map();
 
-	if (!inputArray.length || !stringTerm.length) return [];
-	const docsCount = inputArray.length;
-	
-	inputArray.forEach(({ id, text }) => {
-		const matchingWords = text.toLowerCase().match(regex);
-		const totalWordCount = matchingWords.length;
-		docWordCount.set(id, totalWordCount);
+  if (!inputArray.length || !stringTerm.length) return [];
+  const docsCount = inputArray.length;
 
-		const wordCount = {};
-		matchingWords.forEach(word => {
-			wordCount[word] = (wordCount[word] || 0) + 1;
-		});
-		
-		for (const [word, count] of Object.entries(wordCount)) {
-			const tf = count / totalWordCount;
-			if (!index.has(word)) index.set(word, new Map());
-			index.get(word).set(id, tf);
-		}
-	});
-	/*
-	const avgDocWordCount = Array.from(docWordCount.values())
-		.reduce((acc, cur) => {
-			return acc += cur;
-		}) / docsCount;
-		*/
-	stringTerm.forEach((word) => {
-		const K1 = 2.0;
-		const B = 0.75;
-		const docIds = index.get(word);
-		const idf = docIds ? Math.log2(1 + (docsCount - docIds.size + 1) / (docIds.size + 0.5)) : 0;
-		if (docIds) {
-			docIds.forEach((tf, id) => {
-				//const tfIdf = idf * (tf * (K1 + 1)) / (tf + K1 * ((1 - B) + B * (docWordCount.get(id) / avgDocWordCount)));
-				const tfIdf = idf * tf;
-				result.set(id, (result.get(id) || 0) + tfIdf);
-			});
-		}
+  inputArray.forEach(({ id, text }) => {
+    const matchingWords = text.toLowerCase().match(regex);
+    const totalWordCount = matchingWords.length;
+    docWordCount.set(id, totalWordCount);
 
-	});
-	
-	return Array.from(result.entries())
-		.sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
-		.map(([id]) => id);
+    const wordCount = {};
+    matchingWords.forEach((word) => {
+      wordCount[word] = (wordCount[word] || 0) + 1;
+    });
+
+    Object.entries(wordCount).forEach(([word, count]) => {
+      const tf = count / totalWordCount;
+      if (!index.has(word)) index.set(word, new Map());
+      index.get(word).set(id, tf);
+    });
+  });
+  /*
+const avgDocWordCount = Array.from(docWordCount.values())
+.reduce((acc, cur) => {
+return acc += cur;
+}) / docsCount;
+*/
+  stringTerm.forEach((word) => {
+    // const K1 = 2.0;
+    // const B = 0.75;
+    const docIds = index.get(word);
+    const idf = docIds ? Math.log2(1 + (docsCount - docIds.size + 1) / (docIds.size + 0.5)) : 0;
+    if (docIds) {
+      docIds.forEach((tf, id) => {
+        /* const tfIdf = idf * (tf * (K1 + 1)) / (tf + K1 * ((1 - B) + B *
+(docWordCount.get(id) / avgDocWordCount))); */
+        const tfIdf = idf * tf;
+        result.set(id, (result.get(id) || 0) + tfIdf);
+      });
+    }
+  });
+
+  return Array.from(result.entries())
+    .sort(([, scoreA], [, scoreB]) => scoreB - scoreA)
+    .map(([id]) => id);
 }
